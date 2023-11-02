83d19cc8a4395ca0bd4c75780d3293dbf0ad7897
SalvaMrS <101283370+SalvaMrS@users.noreply.github.com>
2023-11-02 17:28:32 -0300

    .

--- ejercicio4.py
+++ ejercicio4.py
@@ -42,8 +42,63 @@ def generar_embeddings(data: pd.DataFrame, model: callable) -> pd.DataFrame:
 
 df_embeddings = generar_embeddings(df, model)
 
+def calcular_similitud(tensor1: tf.Tensor, tensor2: tf.Tensor) -> float:
+    """
+    Calcula la similitud de coseno entre dos tensores.
+
+    Parámetros:
+    tensor1 (tf.Tensor): El primer tensor.
+    tensor2 (tf.Tensor): El segundo tensor.
+
+    Retorna:
+    float: El valor de similitud de coseno entre los dos tensores.
+    """
+    dot_product = tf.reduce_sum(tf.multiply(tensor1, tensor2))
+    norm1 = tf.norm(tensor1)
+    norm2 = tf.norm(tensor2)
+    cosine_similarity = dot_product / (norm1 * norm2)
+
+    return cosine_similarity
+
+def calcular_centro_vectores(df: pd.DataFrame, categoria: str) -> np.ndarray:
+    """
+    Calcula el centro de los vectores de una categoría específica en un DataFrame.
+
+    Parámetros:
+    df (pd.DataFrame): El DataFrame que contiene los datos de categorías y vectores.
+    categoria (str): El nombre de la categoría para la que se calculará el centro de los vectores.
+
+    Retorna:
+    np.ndarray: Un arreglo NumPy que representa el centro de los vectores de la categoría.
+    """
+    df_categoria = df[df['categoria'] == categoria]
+    centro_categoria = np.mean(df_categoria['vector'].to_list(), axis=0)
+
+    return centro_categoria
+
 # Calcular el centroide de 'Policiales'
 centro_policiales = calcular_centro_vectores(df_embeddings, 'Policiales')
+
+def similitudes_por_categoria(df, centro_vector):
+    """
+    Calcula las similitudes entre un vector central y los datos de todas las categorías en un DataFrame.
+
+    Parámetros:
+    df (pd.DataFrame): El DataFrame que contiene los datos de categorías y vectores.
+    centro_vector (np.ndarray): El vector central con el que se calcularán las similitudes.
+
+    Retorna:
+    dict: Un diccionario donde las claves son las categorías y los valores son las similitudes correspondientes.
+    """
+
+    similitudes_por_categoria = []
+    
+    for categoria in df['categoria'].unique():
+        df_categoria = df[df['categoria'] == categoria]
+        similitudes = [calcular_similitud(centro_vector, vector) for vector in df_categoria['vector'].to_list()]
+        similitudes_por_categoria.append(similitudes)
+
+    return similitudes_por_categoria
 
 data = similitudes_por_categoria(df_embeddings, centro_policiales)
 
